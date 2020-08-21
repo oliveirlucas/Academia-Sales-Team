aedf8759f6607249b6d2b75896d02e90208224a6
unknown <luquinha207@hotmail.com>
2020-08-21 20:05:21 -0300

    Adicionando nova tela de login

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include(realpath(dirname(__FILE__) . "/./php/login.php")); ?>
 
 <!DOCTYPE html>
-<html class="h-100" lang="en">
+<html style="height: 100% !important;" lang="en">
 
 <head>
     <meta charset="utf-8">
@@ -11,12 +11,11 @@
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon.png">
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"> -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style2.css" rel="stylesheet">
 
 </head>
 
-<body class="h-100">
-
+<body style="height: 100% !important; background-image: url('./images/background.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; width: 100%;" >
     <!--*******************
         Preloader start
     ********************-->
@@ -35,9 +34,9 @@
 
 
 
-    <div class="login-form-bg h-100">
-        <div class="container h-100">
-            <div class="row justify-content-center h-100">
+    <div class="login-form-bg" style="height: 100% !important;">
+        <div class="container mr-0" style="height: 100% !important;">
+            <div class="row justify-content-center" style="height: 100% !important;">
                 <div class="col-xl-6">
                     <div class="form-input-content">
                         <div class="card login-form mb-0">
@@ -90,7 +89,8 @@
     <script src="js/custom.min.js"></script>
     <script src="js/settings.js"></script>
     <script src="js/gleek.js"></script>
-    <script src="js/styleSwitcher.js"></script>
+    <style>
+    </style>
 
 </body>
 
